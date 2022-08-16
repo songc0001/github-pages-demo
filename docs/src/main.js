af264985c0622f714139d568d5aa45de54482499
songc0001 <songc0001@gmail.com>
2022-08-16 19:30:36 +0800

    try

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,66 +1,55 @@
-// vue3-sfc-loader
+export const setupAll = () => {
 
-const options = {
-  moduleCache: {
-    vue: Vue
-  },
-  async getFile(url) {
+  // vue3-sfc-loader
 
-    const res = await fetch(url);
-    if (!res.ok)
-      throw Object.assign(new Error(res.statusText + ' ' + url), { res });
-    return {
-      getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
-    }
-  },
-  addStyle(textContent) {
+  const options = {
+    moduleCache: {
+      vue: Vue
+    },
+    async getFile(url) {
 
-    const style = Object.assign(document.createElement('style'), { textContent });
-    const ref = document.head.getElementsByTagName('style')[0] || null;
-    document.head.insertBefore(style, ref);
-  },
-}
+      const res = await fetch(url);
+      if (!res.ok)
+        throw Object.assign(new Error(res.statusText + ' ' + url), { res });
+      return {
+        getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+      }
+    },
+    addStyle(textContent) {
 
-const { loadModule } = window['vue3-sfc-loader'];
+      const style = Object.assign(document.createElement('style'), { textContent });
+      const ref = document.head.getElementsByTagName('style')[0] || null;
+      document.head.insertBefore(style, ref);
+    },
+  }
 
-const { createApp } = Vue
+  const { loadModule } = window['vue3-sfc-loader'];
 
-const app = createApp({
-  components: {
-    'my-try': Vue.defineAsyncComponent(() => loadModule('./App.vue', options))
-  },
-  template: 'Hello<my-try></my-try>'
-});
-
-app.config.compilerOptions.delimiters = ['${', '}']
-
-app.config.errorHandler = (err) => {
-  /* 处理错误 */
-  console.log(err)
-}
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-app.use(ElementPlus)
-
-app.mount('#app');
-
-
-export const setupAll = async () => {
   const { createApp } = Vue
-  const app = createApp({})
+
+  const app = createApp({
+    components: {
+      'my-try': Vue.defineAsyncComponent(() => loadModule('./App.vue', options))
+    },
+    template: 'Hello<my-try></my-try>'
+  });
+
   app.config.compilerOptions.delimiters = ['${', '}']
+
   app.config.errorHandler = (err) => {
     /* 处理错误 */
     console.log(err)
   }
+
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
+
   app.use(ElementPlus)
-  app.mount("#app")
+
+  app.mount('#app');
+
 }
 
+setupAll()
 
