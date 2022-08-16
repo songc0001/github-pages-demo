b93640fa24bbcea32fdff4876278776febab76a3
songc0001 <songc0001@gmail.com>
2022-08-17 01:51:09 +0800

    loaded HelloWorld.vue

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,6 +1,6 @@
-<template>  
+<template>    
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />  
 </template>
 
 <script>
--- docs/src/main.js
+++ docs/src/main.js
@@ -1,9 +1,31 @@
-const setupAll = () => {
+export const setupAll = () => {
 
-  // vue3-sfc-loader
+  // vue3-sfc-loader  
 
   const options = {
-    moduleCache: { vue: Vue },
+    moduleCache: {
+      'vue': Vue,
+      /*'url!'(content, path, type, options) {
+
+        if ( type === '.svg' )
+          return `data:image/svg+xml;base64,${btoa(content)}`;
+        
+        if ( type === '.png' )
+          return `data:image/png;base64,${btoa(content)}`;        
+
+        throw new Error(`${ type } not handled by url!`);
+      },*/
+    },
+    handleModule(type, getContentData, path, options) {
+
+      switch (type) {
+        case '.json': return JSON.parse(source);
+        //case '.svg': return `data:image/svg+xml;base64,${btoa(content)}`;
+        //case '.svg': return 'data:image/svg+xml,' + await getContentData(false);
+        case '.png': return path;        
+        default: return undefined; // let vue3-sfc-loader handle this
+      }
+    },
     async getFile(url) {
 
       const res = await fetch(url);
@@ -14,10 +36,37 @@ const setupAll = () => {
       }
     },
     addStyle(textContent) {
-
       const style = Object.assign(document.createElement('style'), { textContent });
       const ref = document.head.getElementsByTagName('style')[0] || null;
       document.head.insertBefore(style, ref);
+    }, 
+    log(type, ...args) {
+
+      console.log(type, ...args);
+    },
+    compiledCache: {
+      set(key, str) {
+
+        // naive storage space management
+        for (; ;) {
+
+          try {
+
+            // doc: https://developer.mozilla.org/en-US/docs/Web/API/Storage
+            window.localStorage.setItem(key, str);
+            break;
+          } catch (ex) {
+
+            // handle: Uncaught DOMException: Failed to execute 'setItem' on 'Storage': Setting the value of 'XXX' exceeded the quota
+
+            window.localStorage.removeItem(window.localStorage.key(0));
+          }
+        }
+      },
+      get(key) {
+
+        return window.localStorage.getItem(key);
+      },
     },
   }
 
