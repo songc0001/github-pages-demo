6e71ae606c9e23d93faad7cea96615c60914065e
songc0001 <songc0001@gmail.com>
2022-08-17 18:14:52 +0800

    Header Fix

--- docs/src/layout/Layout.vue
+++ docs/src/layout/Layout.vue
@@ -1,15 +1,25 @@
 <template>
   <el-container>
-    <el-header>
-      <!-- NavMeun -->
-      <el-menu mode="horizontal">        
+    <el-header justify="center">
+      <!-- NavMeun -->      
+      <el-menu mode="horizontal" >        
         <!-- 左上個LOGO -->
-        <el-button link>My Web Pages</el-button>
+        <div class="logo">
+          <el-button link>My Web Pages</el-button>
+        </div>
         <el-menu-item index="1">Tools</el-menu-item>
-        <el-menu-item index="2">Blog</el-menu-item>
-      </el-menu>
+        <el-menu-item index="2">Blog</el-menu-item>        
+      </el-menu>      
     </el-header>
 
     <el-main>Main</el-main>
   </el-container>
 </template>
+
+<style scoped>
+.logo{
+  display:flex;
+  vertical-align: middle;
+}
+
+</style>
