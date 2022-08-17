c7679214a631fe6b711d9ebce1b8bc2d973d7f31
songc0001 <songc0001@gmail.com>
2022-08-17 15:37:08 +0800

    Layout Header

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,31 +1,14 @@
 <template>  
-  <el-image style="width: 100px; height: 100px" alt="Vue logo" :src="urls" />
-  <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <Layout />
 </template>
 
 <script>
-import HelloWorldVue from './components/HelloWorld.vue'
+import LayoutVue from './layout/Layout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld: HelloWorldVue
-  },
-  data() {
-    return {
-      urls: './src/assets/img/logo.png'
-    }
+    Layout: LayoutVue
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ docs/src/layout/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-container>
+    <el-header>
+      <!-- NavMeun -->
+      <el-menu mode="horizontal">        
+        <!-- 左上個LOGO -->
+        <el-button link>My Web Pages</el-button>
+        <el-menu-item index="1">Tools</el-menu-item>
+        <el-menu-item index="2">Blog</el-menu-item>
+      </el-menu>
+    </el-header>
+
+    <el-main>Main</el-main>
+  </el-container>
+</template>
